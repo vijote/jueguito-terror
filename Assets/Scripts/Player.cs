65a7ac153dc03f74fed617cb0ae2dbb42eca608e
Juan Dominguez <juanignaciodom3@gmail.com>
2023-07-15 21:33:59 -0300

    add down left and right animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,7 +5,9 @@ public class Player : MonoBehaviour
     public float speed = 5f;
     private Animator animator;
 
-    private string state;
+    private string state = "Idle";
+    private string verticalDirection = "Down";
+    private string horizontalDirection = "Right";
 
     // Start is called before the first frame update
     void Start()
@@ -30,14 +30,40 @@ public class Player : MonoBehaviour
             return;
         };
 
-        transform.Translate(Vector2.right * speed * horizontalMovement * Time.deltaTime);
-        transform.Translate(Vector2.up * speed * verticalMovement * Time.deltaTime);
+        if (horizontalMovement == 0) horizontalDirection = null;
+
+        if (verticalMovement == 0) verticalDirection = null;
+
+        if (horizontalMovement > 0)
+        {
+            horizontalDirection = "Right";
+        } else if (horizontalMovement < 0)
+        {
+            horizontalDirection = "Left";
+        }
+
+        if (verticalMovement > 0)
+        {
+            verticalDirection = "Up";
+        }
+        else if (verticalMovement < 0)
+        {
+            verticalDirection = "Down";
+        }
+
+        transform.Translate(speed * Time.deltaTime * horizontalMovement  * Vector2.right);
+        transform.Translate(speed * Time.deltaTime * verticalMovement * Vector2.up);
         state = "Walk";
     }
 
     private void Animate()
     {
+        string animationName = state;
 
-        animator.Play(state);
+        if(verticalDirection != null) animationName += verticalDirection;
+
+        if (horizontalDirection != null) animationName += horizontalDirection;
+
+        animator.Play(animationName);
     }
 }
